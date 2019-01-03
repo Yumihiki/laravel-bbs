af41ff81f5545d4ab1b1262a51aec422dc7baf62
Yumihiki <nibu10.majik@giaml.com>
2019-01-03 17:42:37 +0900

    ページネーションの実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,5 +35,8 @@
                 </div>
             </div>
         @endforeach
+        <div class="d-flex justify-content-center mb-5">
+            {{ $posts->links() }}
+        </div>
     </div>
 @endsection
